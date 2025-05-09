68b825f6ba24feef4aa1b50a8891f9ef59148ac7
ViktoriiaHudym <vh@jooble.com>
2025-05-09 22:32:29 +0300

    add corsheaders support

--- MistoQuest/mistoquest_backend/settings.py
+++ MistoQuest/mistoquest_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
+    'corsheaders',
     # 'dj_rest_auth',
 
     'rest_framework',
@@ -70,6 +71,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -98,7 +100,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mistoquest_backend.wsgi.application'
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
