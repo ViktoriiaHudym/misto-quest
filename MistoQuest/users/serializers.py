87546399d3ff09a96683e60a7c9e34aa0bd102e5
ViktoriiaHudym <vh@jooble.com>
2025-07-20 19:53:28 +0300

    rewrite user views with classes; add UserProfilesView; update urls

--- MistoQuest/users/serializers.py
+++ MistoQuest/users/serializers.py
@@ -1,22 +1,27 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
+
 from .models import UserStats
 
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ['id', 'username', 'email']
-
-
-class RegisterSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     username = serializers.CharField(required=True)
     password = serializers.CharField(write_only=True, required=True)
 
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'email', 'password']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=get_user_model().objects.all(),
+                fields=['username', 'email']
+            )
+        ]
+
     def create(self, validated_data):
-        User = get_user_model()
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
--- MistoQuest/users/views.py
+++ MistoQuest/users/views.py
@@ -1,35 +1,75 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 
-from .serializers import UserSerializer, UserStatsSerializer, RegisterSerializer
+from django.contrib.auth import get_user_model
+
+from .serializers import UserSerializer, UserStatsSerializer
 from .models import UserStats
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def get_profile(request):
-    user = request.user
-    serializer = UserSerializer(user, many=False)
-    return Response(serializer.data)
+class RegisterUserProfileView(APIView):
+    """
+    API View to register a new user profile.
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=ValueError):
+            serializer.create(validated_data=request.data)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            {
+                "error": True,
+                "error_msg": serializer.error_messages,
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 
-@api_view(['GET'])
-def get_statistics(request, username):
-    try:
-        user_stat = UserStats.objects.get(id_user__username=username)
-        serializer = UserStatsSerializer(user_stat, many=False)
+class UserProfileView(APIView):
+    """
+    API View to get the user profile.
+    """
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user, many=False)
         return Response(serializer.data)
-    except UserStats.DoesNotExist:
-        return Response({"error": "Stats not found for this user"}, status=status.HTTP_404_NOT_FOUND)
 
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def register(request):
-    serializer = RegisterSerializer(data=request.data)
-    if serializer.is_valid():
-        user = serializer.save()
-        return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserProfilesView(APIView):
+    """
+    API View to get all user profiles.
+    """
+    permission_classes = [IsAdminUser]
+
+    def get(self, request):
+        users = get_user_model().objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+class UserStatsView(APIView):
+    """
+    API View to get the user statistics.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        try:
+            user_stat = UserStats.objects.get(id_user=request.user)
+            serializer = UserStatsSerializer(user_stat, many=False)
+            return Response(serializer.data)
+        except UserStats.DoesNotExist:
+            return Response(
+                {"error": "Stats not found for this user"},
+                status=status.HTTP_404_NOT_FOUND
+            )
